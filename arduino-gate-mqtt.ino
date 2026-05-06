7d4de04ca4c79405a4139de058fde52fb151f2dc
Marek Vach <marek@vach.cz>
2026-05-07 00:38:22 +0200

    Change electric circuit

--- arduino-gate-mqtt.ino
+++ arduino-gate-mqtt.ino
@@ -29,26 +29,26 @@
 // MQTT topics
 const char* topic_connect_status                 = "g/connect";
 const char* topic_mailbox                        = "g/mail";
-const char* topic_gate_mode                      = "g/mode";          // 0 = Opened, 1 = Closed, 2 = Opening, 3 = Closing, 4 = Offline
+const char* topic_motor_running                  = "g/m/run";
 const char* topic_relay_close_set                = "g/cl/set";
 const char* topic_relay_close_response           = "g/cl/resp";
 const char* topic_relay_open_car_set             = "g/op_car/set";
 const char* topic_relay_open_car_response        = "g/op_car/resp";
-const char* topic_relay_open_pedestrian_set      = "g/op_ped/set";
-const char* topic_relay_open_pedestrian_response = "g/op_ped/resp";
-const char* topic_button_car                     = "g/b/car";
-const char* topic_button_pedestrian              = "g/b/ped";
+//const char* topic_relay_open_pedestrian_set      = "g/op_ped/set";
+//const char* topic_relay_open_pedestrian_response = "g/op_ped/resp";
+const char* topic_limiter_closed                 = "g/l/clsd";
+const char* topic_limiter_opened                 = "g/l/opnd";
 const char* topic_photocell_outside              = "g/ph/out";
 const char* topic_photocell_inside               = "g/ph/in";
 const char* topic_induction_loop                 = "g/ind";
-const char* topic_radio_open                     = "g/r/op";
-const char* topic_radio_close                    = "g/r/cl";
+const char* topic_open_pulse                     = "g/o/p";
+const char* topic_open_automatic                 = "g/o/a";
 
 
 
 // values for your network.
 byte mac[]    = { 0x02, 0x17, 0x3A, 0x4B, 0x5C, 0x6E };
-const char* mqtt_server      = "192.168.0.2"; // used for MQTT server and for check connectivity = if webserver is running on same machine at port 80
+const char* mqtt_server      = "192.168.0.8"; // used for MQTT server and for check connectivity = if webserver is running on same machine at port 80
 const int   mqtt_port        = 1883;
 const char* mqtt_name        = "garduino";
 const char* mqtt_user        = "gate";
@@ -69,38 +69,38 @@ char received_message[MAX_PAYLOAD_LEN];
 
 // HW pinout section
 const int pin_sensor_mailbox = 2;
-const int pin_sensor_closed = 3;
+const int pin_motor_running = 3;
 
 // Relay module **never use more relay then one at same time because of internal Arduino power supply will overload
 // pin 4 reserved for chip select of SD card.
-const int pin_relay_close = 5;
+//const int pin_relay_close = 5;
 const int pin_relay_open_car = 6;
-const int pin_relay_open_pedestrian = 7;
+//const int pin_relay_open_pedestrian = 7;
 
-const int pin_button_car = 8;
-const int pin_button_pedestrian = 9;
+const int pin_limiter_closed = 8;
+const int pin_limiter_opened = 9;
 // pin 10 reserved for chip select of Ethernet W5100
 
 const int pin_photocell_outside = A0;
 const int pin_photocell_inside = A1;
 const int pin_induction_loop = A2;
 
-const int pin_radio_open = A3;
-const int pin_radio_close = A4;
+const int pin_open_pulse = A3;
+const int pin_open_automatic = A4;
 // A5 pin free for future use
 
 
 
 // Variables to memorize last states
 int last_sensor_mailbox;
-int last_sensor_closed;
-int last_button_car;
-int last_button_pedestrian;
+int last_motor_running;
+int last_limiter_closed;
+int last_limiter_opened;
 int last_photocell_outside;
 int last_photocell_inside;
 int last_induction_loop;
-int last_radio_open;
-int last_radio_close;
+int last_open_pulse;
+int last_open_automatic;
 
 int input_state;
 
@@ -149,9 +149,9 @@ void reconnectMQTT() {
 
     MqttPeriodicReport();
 
-    mqttClient.subscribe(topic_relay_close_set);
+    //mqttClient.subscribe(topic_relay_close_set);
     mqttClient.subscribe(topic_relay_open_car_set);
-    mqttClient.subscribe(topic_relay_open_pedestrian_set);
+    //mqttClient.subscribe(topic_relay_open_pedestrian_set);
   } 
   else {
     Serial.print(F("error - MQTT library code is: "));
@@ -167,8 +167,8 @@ void MqttPeriodicReport() {
   mqttClient.publish (topic_mailbox, input_state ? "1" : "0");
 
   if (gate_is_moving == false) {
-    input_state = digitalRead( pin_sensor_closed );
-    mqttClient.publish (topic_gate_mode, input_state ? "1" : "0");
+    input_state = digitalRead( pin_motor_running );
+    mqttClient.publish (topic_motor_running, input_state ? "1" : "0");
   }
 }
 
@@ -214,69 +214,69 @@ void checkAndRepairConnectivity() {
 
 void setupIoPins() {
   pinMode(pin_sensor_mailbox, INPUT_PULLUP);
-  pinMode(pin_sensor_closed,  INPUT_PULLUP);
+  pinMode(pin_motor_running,  INPUT);
 
-  pinMode(pin_relay_close,           OUTPUT);
+  //pinMode(pin_relay_close,           OUTPUT);
   pinMode(pin_relay_open_car,        OUTPUT);
-  pinMode(pin_relay_open_pedestrian, OUTPUT);
+  //pinMode(pin_relay_open_pedestrian, OUTPUT);
 
-  pinMode(pin_button_car,        INPUT_PULLUP);
-  pinMode(pin_button_pedestrian, INPUT_PULLUP);
+  pinMode(pin_limiter_closed,        INPUT);
+  pinMode(pin_limiter_opened, INPUT);
 
-  pinMode(pin_photocell_outside, INPUT_PULLUP);
-  pinMode(pin_photocell_inside,  INPUT_PULLUP);
-  pinMode(pin_induction_loop,    INPUT_PULLUP);
+  pinMode(pin_photocell_outside, INPUT);
+  pinMode(pin_photocell_inside,  INPUT);
+  pinMode(pin_induction_loop,    INPUT);
 
-  pinMode(pin_radio_open,  INPUT_PULLUP);
-  pinMode(pin_radio_close, INPUT_PULLUP);
+  pinMode(pin_open_pulse,  INPUT);
+  pinMode(pin_open_automatic, INPUT);
 
   turnRelaysOff();
 
   last_sensor_mailbox    = digitalRead(pin_sensor_mailbox);
-  last_sensor_closed     = digitalRead(pin_sensor_closed);
-  last_button_car        = digitalRead(pin_button_car);
-  last_button_pedestrian = digitalRead(pin_button_pedestrian);
+  last_motor_running     = digitalRead(pin_motor_running);
+  last_limiter_closed    = digitalRead(pin_limiter_closed);
+  last_limiter_opened    = digitalRead(pin_limiter_opened);
   last_photocell_outside = digitalRead(pin_photocell_outside);
   last_photocell_inside  = digitalRead(pin_photocell_inside);
   last_induction_loop    = digitalRead(pin_induction_loop);
-  last_radio_open        = digitalRead(pin_radio_open);
-  last_radio_close       = digitalRead(pin_radio_close);
+  last_open_pulse        = digitalRead(pin_open_pulse);
+  last_open_automatic    = digitalRead(pin_open_automatic);
 }
 
 
 void checkInputsForChanges() {
   const int input_pins[9]    = {
                                 pin_sensor_mailbox, 
-                                pin_sensor_closed, 
-                                pin_button_car, 
-                                pin_button_pedestrian, 
+                                pin_motor_running, 
+                                pin_limiter_closed, 
+                                pin_limiter_opened, 
                                 pin_photocell_outside, 
                                 pin_photocell_inside, 
                                 pin_induction_loop, 
-                                pin_radio_open, 
-                                pin_radio_close
+                                pin_open_pulse, 
+                                pin_open_automatic
                               };
   int* last_inputs[9]        = {
                                 &last_sensor_mailbox, 
-                                &last_sensor_closed, 
-                                &last_button_car, 
-                                &last_button_pedestrian, 
+                                &last_motor_running, 
+                                &last_limiter_closed, 
+                                &last_limiter_opened, 
                                 &last_photocell_outside, 
                                 &last_photocell_inside, 
                                 &last_induction_loop, 
-                                &last_radio_open, 
-                                &last_radio_close
+                                &last_open_pulse, 
+                                &last_open_automatic
                               };
   const char* mqtt_topics[9] = {
                                 topic_mailbox, 
-                                topic_gate_mode, 
-                                topic_button_car, 
-                                topic_button_pedestrian, 
+                                topic_motor_running, 
+                                topic_limiter_closed, 
+                                topic_limiter_opened, 
                                 topic_photocell_outside, 
                                 topic_photocell_inside, 
                                 topic_induction_loop, 
-                                topic_radio_open, 
-                                topic_radio_close
+                                topic_open_pulse, 
+                                topic_open_automatic
                               };
 
   for (int i = 0; i < 9; i++) {
@@ -297,19 +297,19 @@ void checkInputsForChanges() {
 
 
 void turnRelaysOff() {
-  digitalWrite(pin_relay_close,           LOW);
+  //digitalWrite(pin_relay_close,           LOW);
   digitalWrite(pin_relay_open_car,        LOW);
-  digitalWrite(pin_relay_open_pedestrian, LOW);
+  //digitalWrite(pin_relay_open_pedestrian, LOW);
 
-  mqttClient.publish(topic_relay_close_response, "0");
+  //mqttClient.publish(topic_relay_close_response, "0");
   mqttClient.publish(topic_relay_open_car_response, "0");
-  mqttClient.publish(topic_relay_open_pedestrian_response, "0");
+  //mqttClient.publish(topic_relay_open_pedestrian_response, "0");
 }
 
 
 void gateIsMoving() {
   gate_is_moving = false;
-  last_sensor_closed = 2; // force refresh of input in next comparing cycle
+  last_motor_running = 2; // force refresh of input in next comparing cycle
 }
 
 
@@ -364,28 +364,10 @@ void loop() {
         clearMessages();
       } 
 
-      if (strcmp(received_topic, topic_relay_close_set) == 0) {
-          mqttClient.publish(topic_relay_close_response, "1");
-          mqttClient.publish(topic_gate_mode, "3"); 
-          digitalWrite(pin_relay_close, HIGH);
-          timer_relays_off.start();
-          timer_gate_is_moving.start();
-          gate_is_moving = true;
-      }
-
       if (strcmp(received_topic, topic_relay_open_car_set) == 0) {
           mqttClient.publish(topic_relay_open_car_response, "1");
-          mqttClient.publish(topic_gate_mode, "2"); 
+          mqttClient.publish(topic_motor_running, "2"); 
           digitalWrite(pin_relay_open_car, HIGH);
-          timer_relays_off.start();
-          timer_gate_is_moving.start();
-          gate_is_moving = true;
-      }
-
-      if (strcmp(received_topic, topic_relay_open_pedestrian_set) == 0) {
-          mqttClient.publish(topic_relay_open_pedestrian_response, "1");
-          mqttClient.publish(topic_gate_mode, "2"); 
-          digitalWrite(pin_relay_open_pedestrian, HIGH);
           timer_relays_off.start();
           timer_gate_is_moving.start();
           gate_is_moving = true;
