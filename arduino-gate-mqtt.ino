e1dda0351e769917d360c2e5658a03ddd23bf0fb
Marek Vach <marek@vach.cz>
2026-05-17 23:00:58 +0200

    Testing new logic without automatic

--- arduino-gate-mqtt.ino
+++ arduino-gate-mqtt.ino
@@ -103,7 +103,7 @@ boolean reconnectEthernet() {
   delay(1500);
 
   if (Ethernet.localIP() != IPAddress(0,0,0,0)) {
-    Serial.print(F("Success! Have "));
+    Serial.print(F("New IP "));
     Serial.println(Ethernet.localIP());
     return true;
   }
@@ -127,21 +127,24 @@ void reconnectMQTT() {
   mqttClient.setKeepAlive(30);
   mqttClient.setTimeout(40);
 
-  Serial.println(F("Connecting MQTT..."));
+  Serial.println(F("Cnctg MQTT..."));
   mqttClient.connect(mqtt_name, mqtt_user, mqtt_password);
   delay(1500);
 
   if (mqttClient.connected()){
-    Serial.print(F("MQTT connected to server "));
+    Serial.print(F("MQTT cnctd to srv "));
     Serial.println(mqtt_server);
 
     MqttPeriodicReport();
+    if ( millis() < 10000 )
+      mqttClient.publish(topic_gate_position, returnGatePosition());
+    
 
     mqttClient.subscribe(topic_relay_open_pulse);
     mqttClient.subscribe(topic_relay_open_automatic);
   } 
   else {
-    Serial.print(F("error - MQTT library code is: "));
+    Serial.print(F("error - MQTT lib code is: "));
     Serial.println(mqttClient.lastError());
   } 
 } 
@@ -182,12 +185,12 @@ void checkAndRepairConnectivity() {
         }
       }
       else {
-        Serial.println(F("Ethernet connection OK."));
+        Serial.println(F("Eth OK."));
         reconnectMQTT();
       }
     }
     else
-      Serial.println(F("MQTT connection OK."));
+      Serial.println(F("MQTT OK."));
 }
 
 
@@ -217,21 +220,20 @@ void setupIoPins() {
   last_input_open_automatic = digitalRead(pin_input_open_automatic);
   last_sensor_mailbox       = digitalRead(pin_sensor_mailbox);
   last_home_ring            = digitalRead(pin_home_ring);
+  last_gate_position        = returnGatePosition();
 }
 
 
 
 int returnGatePosition() {    // 1=running, 2=closed, 3=opened, 4=unkown_position 5=auto_close
   if ( digitalRead(pin_motor_running) == 1 )
-     input_state = 3;
+      return 3;
   else if ( digitalRead(pin_limiter_closed) == 1 )
-    input_state = 1;
+      return 1;
   else if ( digitalRead(pin_limiter_opened) == 1 )
-    input_state = 2;
+      return 2;
   else
-    input_state = 4;
-
-  return input_state;
+      return 4;
 }
 
 
@@ -247,7 +249,7 @@ void checkInputsForChanges() {
                                         pin_input_open_automatic,
                                         pin_sensor_mailbox, 
                                         pin_home_ring,
-                                        gate_position  // not pin, calculated variable, must be last in array
+                                        100
                                        };
   int* last_inputs[key_count]        = {
                                         &last_photocell_outside, 
@@ -271,10 +273,11 @@ void checkInputsForChanges() {
                                        };
 
   for (int i = 0; i < key_count; i++) {
-
-    if ( i == key_count )
-
-    input_state = digitalRead ( input_pins[i] );
+    if ( i == key_count - 1 )
+      input_state = returnGatePosition();
+    else
+      input_state = digitalRead ( input_pins[i] );
+    
     if ( input_state != *last_inputs[i] ) {
       *last_inputs[i] = input_state;
       mqttClient.publish (mqtt_topics[i], input_state ? "1" : "0");
@@ -295,11 +298,31 @@ void clearMessages() {
 
 
 
+void makeOpenGatePulse() {
+  digitalWrite(pin_relay_open, HIGH);
+  timer_turn_relays_off.start();
+}
+
+
+
+void makeOpenGateAutomatic() {
+
+}
+
+
+
+void turnRelaysOff() {
+  digitalWrite(pin_relay_open, LOW);
+}
+
+
+
 Ticker timer_check_connectivity(checkAndRepairConnectivity, 120000);         // cals function every 120s
 Ticker timer_maintain_ethernet(maintainEthernet, 1200000);                   // 20min
 Ticker timer_maintain_mqtt(maintainMQTT, 1000);                              // 1s
 Ticker timer_check_inputs(checkInputsForChanges, 100);                       // 0.1s
 Ticker timer_mqtt_periodic_report(MqttPeriodicReport, 300000);               // 5min
+Ticker timer_turn_relays_off(turnRelaysOff, 1000, 1);                        // 1s, repeated once
 
 
 void setup() {
@@ -323,6 +346,7 @@ void loop() {
   timer_maintain_mqtt.update();
   timer_check_inputs.update();
   timer_mqtt_periodic_report.update();
+  timer_turn_relays_off.update();
 
   if (received_message[0] != '\0') {
       Serial.print(F("Incoming MQTT: "));
@@ -332,10 +356,7 @@ void loop() {
 
 
       if (strcmp(received_topic, topic_relay_open_pulse) == 0) {
-          digitalWrite(pin_relay_open_car, HIGH);
-          timer_relays_off.start();
-          timer_gate_is_moving.start();
-          gate_is_moving = true;
+        makeOpenGatePulse();
       }
 
       clearMessages();
