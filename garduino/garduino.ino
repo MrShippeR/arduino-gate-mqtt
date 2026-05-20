9b3a1176b737392a6bff5bee183c4291fb944790
Marek Vach <marek@vach.cz>
2026-05-20 20:41:23 +0200

    added ind loop autoopen setting mqqt part

--- garduino/garduino.ino
+++ garduino/garduino.ino
@@ -13,8 +13,7 @@
 * Užitečná dokumentace:
 * https://www.home-assistant.io/integrations/sensor.mqtt/
 * https://www.home-assistant.io/integrations/switch.mqtt/
-* https://github.com/256dpi/arduino-mqtt
-* https://github.com/sstaub/Ticker
+* https://github.com/256dpi/arduino-mqtt lwmqtt
 *
 * Verze 1.0 09/2025
 * marek@vach.cz
@@ -22,6 +21,7 @@
 
 #include <Ethernet.h>
 #include <MQTT.h>
+#include <EEPROM.h>
 
 
 // HW pinout section
@@ -43,7 +43,6 @@ const int pin_home_ring = A5;
 
 // values for network
 byte mac[] = {0x02, 0x17, 0x3A, 0x4B, 0x5C, 0x6E};
-byte ip[] = {192, 168, 20, 7};
 const char* mqtt_server   = "192.168.0.8";
 const char* mqtt_name     = "garduino";
 const char* mqtt_password = "Drainpipe";
@@ -60,6 +59,8 @@ const char* topic_gate_position                  = "g/p";
 const char* topic_relay_open_pulse               = "g/o";
 const char* topic_relay_open_automatic           = "g/oat";
 const char* topic_input_open_automatic           = "g/i/oat";
+const char* topic_setting_loop_autoopen_set      = "g/i/s";
+const char* topic_setting_loop_autoopen_info     = "g/i/i";
 const char* topic_mailbox                        = "d/m";
 const char* topic_home_ring                      = "d/r";
 
@@ -72,7 +73,9 @@ const unsigned long period_mqtt_msg = 30000; // 30s
 const unsigned long period_relay_pulse = 1000; // 1s
 const unsigned long period_max_waiting_autoclose = 600000; // 10min 600000
 const unsigned long period_delay_autoclose = 3000; // 3s
-const unsigned long period_scan_inputs = 250; // ms
+const unsigned long period_fast_scan_inputs = 250; // ms
+const unsigned long period_slow_scan_inputs = 1100; // 1,1s
+const unsigned long period_message_clear_way = 2000; // 2s
 const char* gate_positions_texts[] = {
                                       "v pohybu",
                                       "zavreno",
@@ -82,6 +85,7 @@ const char* gate_positions_texts[] = {
                                       "nedefinovano"
 };
 const char* text_changed_state_to = " changed state to ";
+const char* text_autoclose_canceled = "Autoclose canceled.";
 
 // Global variables
 byte index_gate_position = 5;
@@ -92,7 +96,10 @@ unsigned long timing_for_cancel_autoclose = 0;
 bool autoclose_activated = 0;
 bool autoclose_planned_close_signal = 0;
 unsigned long timing_delay_autoclose = 0;
-unsigned long timing_scan_inputs_for_change = 0;
+unsigned long timing_fast_scan_inputs = 0;
+unsigned long timing_slow_scan_inputs = 0;
+unsigned long timing_message_clear_way = 0;
+bool setting_loop_autoopen;
 
 // Variables to memorize last states
 int last_sensor_mailbox;
@@ -100,9 +107,9 @@ int last_home_ring;
 int last_input_open_automatic;
 
 
+
 void connectMqtt() {
   Serial.print(F("MQTT connect"));
-
   mqttClient.disconnect();
   Ethernet.maintain();
 
@@ -125,9 +132,10 @@ void connectMqtt() {
 
     mqttClient.subscribe(topic_relay_open_pulse);
     mqttClient.subscribe(topic_relay_open_automatic);
+    mqttClient.subscribe(topic_setting_loop_autoopen_set);
   }
   else
-    Serial.println(F("MQTT didn't connect!"));
+    Serial.println(F("error"));
 
 }
 
@@ -178,7 +186,7 @@ void makeOpenGatePulse() {
 
 void makeOpenGateAutomatic() {
   if ( autoclose_activated == 1 ) {
-    Serial.println(F("Canceling autoclose."));
+    Serial.println(text_autoclose_canceled);
     autoclose_activated = 0;
     autoclose_planned_close_signal = 0;
     return;
@@ -194,21 +202,12 @@ void makeOpenGateAutomatic() {
 
   autoclose_activated = 1;
   timing_for_cancel_autoclose = millis();
-  //timing_delay_autoclose = 0;  // maximum of unsigned long type
-  Serial.println(F("Waiting max 10min for vehicle to pass thru."));
+  Serial.println(F("10min started."));
 }
 
 
 
-void checkInputsForChanges() {
-  if (digitalRead(pin_sensor_mailbox) != last_sensor_mailbox ) {
-    last_sensor_mailbox = digitalRead(pin_sensor_mailbox);
-    Serial.print(topic_mailbox);
-    Serial.print(text_changed_state_to);
-    Serial.println(last_sensor_mailbox);
-    mqttClient.publish (topic_mailbox, last_sensor_mailbox);
-  }
-
+void fastScanInputs() {
   if (digitalRead(pin_home_ring) != last_home_ring ) {
     last_home_ring = digitalRead(pin_home_ring);
     Serial.print(topic_home_ring);
@@ -221,6 +220,18 @@ void checkInputsForChanges() {
     index_gate_position = returnGatePosition();
     mqttClient.publish(topic_gate_position, gate_positions_texts[index_gate_position]);
   } 
+}
+
+
+
+void slowScanInputs() {
+  if (digitalRead(pin_sensor_mailbox) != last_sensor_mailbox ) {
+    last_sensor_mailbox = digitalRead(pin_sensor_mailbox);
+    Serial.print(topic_mailbox);
+    Serial.print(text_changed_state_to);
+    Serial.println(last_sensor_mailbox);
+    mqttClient.publish (topic_mailbox, last_sensor_mailbox);
+  }
 
   if (digitalRead(pin_input_open_automatic) != last_input_open_automatic) {
     last_input_open_automatic = digitalRead(pin_input_open_automatic);
@@ -269,6 +280,9 @@ void setup() {
   Serial.println(Ethernet.localIP());
   delay(500);
   connectMqtt();
+
+  setting_loop_autoopen = EEPROM.read(0);
+  mqttClient.publish(topic_setting_loop_autoopen_info, setting_loop_autoopen ? "1" : "0");
 }
 
 
@@ -288,9 +302,11 @@ void loop() {
     mqttClient.publish(topic_gate_position, gate_positions_texts[index_gate_position]);
   }
 
-  if ( millis() - timing_scan_inputs_for_change > period_scan_inputs )
-    checkInputsForChanges();
+  if ( millis() - timing_fast_scan_inputs > period_fast_scan_inputs )
+    fastScanInputs();
   
+  if ( millis() - timing_slow_scan_inputs > period_slow_scan_inputs )
+    slowScanInputs();
 
   if ( (millis() - timing_for_relay_pulse > period_relay_pulse) && relay_open_active == 1 ) {
     relay_open_active = 0;
@@ -300,15 +316,18 @@ void loop() {
   if ( (millis() - timing_for_cancel_autoclose > period_max_waiting_autoclose) && autoclose_activated == 1 ) {
     autoclose_activated = 0;  
     autoclose_planned_close_signal = 0;
-    Serial.println(F("Autoclose canceled."));
+    Serial.println(text_autoclose_canceled);
     index_gate_position = returnGatePosition();
     mqttClient.publish(topic_gate_position, gate_positions_texts[index_gate_position]);
   }
 
   if ( (millis() - timing_delay_autoclose > period_delay_autoclose) && autoclose_activated == 1 && autoclose_planned_close_signal == 1 ) {
-    Serial.println(F("Waiting for clear way."));
+    
+    if ( millis() - timing_message_clear_way > period_message_clear_way ) {
+      timing_message_clear_way = millis();
+      Serial.println(F("Waiting for clear way."));
+    }
     if ( digitalRead(pin_induction_loop) == 0 && digitalRead(pin_photocell_outside) == 0 && digitalRead(pin_photocell_inside) == 0 ) {
-      Serial.println(F("Autoclose started."));
       autoclose_activated = 0;
       makeOpenGatePulse();
       autoclose_planned_close_signal = 0;
@@ -331,6 +350,21 @@ void loop() {
         makeOpenGateAutomatic();
       }
 
+      if (strcmp(received_topic, topic_setting_loop_autoopen_set) == 0) {
+        if (received_message[0] == '1')
+          setting_loop_autoopen = 1;
+        else
+          setting_loop_autoopen = 0;
+
+        EEPROM.update(0, setting_loop_autoopen);
+        Serial.print(F("EEPROM "));
+        Serial.print(topic_setting_loop_autoopen_set);
+        Serial.print(F(" updated to "));
+        Serial.println(setting_loop_autoopen);
+        mqttClient.publish(topic_setting_loop_autoopen_info, setting_loop_autoopen ? "1" : "0");
+      }
+      
+
       clearMessages();
   }
 
@@ -339,13 +373,13 @@ void loop() {
   if ( autoclose_activated == 1 && digitalRead(pin_induction_loop) == 1 && autoclose_planned_close_signal == 0 ) {
     autoclose_activated = 0;
     autoclose_planned_close_signal = 1;
-    Serial.println(F("Closing behind vehicle."));
+    Serial.println(F("Vehicle on loop."));
   }
     
   if ( autoclose_activated == 0 && digitalRead(pin_induction_loop) == 0 && autoclose_planned_close_signal == 1 ) {
     autoclose_activated = 1;
     timing_delay_autoclose = millis();
-    Serial.println(F("delay started"));
+    Serial.println(F("Delay started."));
   }
     
   
